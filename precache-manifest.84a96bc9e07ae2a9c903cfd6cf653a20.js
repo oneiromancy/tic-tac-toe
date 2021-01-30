39a5d54bc3f78c023e76865e912158902ceecfcf
Henri <oneiromancy.ai@gmail.com>
2021-01-30 21:51:21 +0000

    Updates

---
+++ precache-manifest.84a96bc9e07ae2a9c903cfd6cf653a20.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "28d514634aaa531bb50e",
+    "url": "/tic-tac-toe/static/css/main.0fd3bdbe.chunk.css"
+  },
+  {
+    "revision": "28d514634aaa531bb50e",
+    "url": "/tic-tac-toe/static/js/main.28d51463.chunk.js"
+  },
+  {
+    "revision": "29ef38af3dd5062d1ecb",
+    "url": "/tic-tac-toe/static/js/1.29ef38af.chunk.js"
+  },
+  {
+    "revision": "26ead58317a649cdf406",
+    "url": "/tic-tac-toe/static/js/runtime~main.26ead583.js"
+  },
+  {
+    "revision": "35a6a229c9c5e656d86793b0a303089e",
+    "url": "/tic-tac-toe/index.html"
+  }
+];
